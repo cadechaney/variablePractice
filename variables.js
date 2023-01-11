23ce5fa29d395d8fea4ce2682a667d22db211a5f
Cade Chaney <cadechaney@live.com>
2023-01-11 12:56:44 -0600

    Add console.log statements

--- variables.js
+++ variables.js
@@ -4,11 +4,19 @@ var email = "helloworld@gmail.com";
 var zipCode = "62304"
 
 //number variables
-var age = 20
-var startingAge = 22
-var weight = 175
+var age = 20;
+var startingAge = 22;
+var weight = 175;
 
 //boolean variables
-var depositPaid = true
-var working = true
-var studying = true
+var depositPaid = true;
+var working = true;
+var studying = true;
+
+name = "Max";
+email = "goodbyeworld@gmail.com";
+zipCode = "45098";
+
+console.log(name);
+console.log(depositPaid);
+console.log(email);
